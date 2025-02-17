14e2d110bf55565e3c6fecb7dbda4a2e2da6fabd
Castyan <120135015+Castyan@users.noreply.github.com>
2025-02-17 15:05:20 +0100

    main.tf -> ajout id du projet + commentaires

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,33 @@
+# Configuration du fournisseur Google Cloud
 provider "google" {
-  project = "your-project-id"
-  region  = "europe-west1"
+  project = "bionic-union-451212-m1"  # ID du projet Google Cloud
+  region  = "europe-west9"            # Région Google Cloud
 }
 
+# Ressource pour créer une instance de machine virtuelle Google Compute Engine
 resource "google_compute_instance" "minecraft_vm" {
-  name         = "minecraft-server"
-  machine_type = "e2-medium"
-  zone         = "europe-west1-b"
-  image        = "projects/your-project-id/global/images/minecraft-server-image"
+  name         = "minecraft-server"   # Nom de l'instance
+  machine_type = "e2-medium"          # Type de machine
+  zone         = "europe-west1-b"     # Zone de l'instance
+  image        = "projects/your-project-id/global/images/minecraft-server-image"  # Image de l'instance
 
+  # Configuration de l'interface réseau
   network_interface {
-    network = "default"
+    network = "default"  # Réseau par défaut
     access_config {
-      nat_ip = google_compute_address.minecraft_ip.address
+      nat_ip = google_compute_address.minecraft_ip.address  # Adresse IP NAT
     }
   }
 
-  tags = ["minecraft"]
+  tags = ["minecraft"]  # Tags pour l'instance
 }
 
+# Ressource pour créer une adresse IP statique
 resource "google_compute_address" "minecraft_ip" {
-  name = "minecraft-ip"
+  name = "minecraft-ip"  # Nom de l'adresse IP
 }
 
+# Ressource pour ajouter l'adresse IP à Firestore via une fonction Cloud
 resource "null_resource" "add_ip_to_firestore" {
   provisioner "local-exec" {
     command = <<EOT
@@ -33,10 +38,11 @@ resource "null_resource" "add_ip_to_firestore" {
   }
 
   depends_on = [
-    google_compute_instance.minecraft_vm
+    google_compute_instance.minecraft_vm  # Dépendance à l'instance Minecraft
   ]
 }
 
+# Sortie de l'adresse IP publique de l'instance Minecraft
 output "minecraft_vm_public_ip" {
-  value = google_compute_address.minecraft_ip.address
+  value = google_compute_address.minecraft_ip.address  # Valeur de l'adresse IP
 }
