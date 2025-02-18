c7d805634542a5ee4697185ca385b5065086e2fb
BaptisteMorvilliers <baptiste.morvilliers@gmail.com>
2025-02-18 11:19:21 +0100

    mise à jour output

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,3 @@
 output "minecraft_vm_public_ip" {
-  value = google_compute_address.minecraft_ip.address
+  value = google_compute_instance.minecraft_vm.network_interface[0].access_config[0].nat_ip
 }
