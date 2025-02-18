cbdb400c55348a99e493ca5cc54796a79ac150ca
BaptisteMorvilliers <baptiste.morvilliers@gmail.com>
2025-02-18 11:18:12 +0100

    modification output

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,6 +26,4 @@ resource "google_compute_address" "minecraft_ip" {
   name = "minecraft-ip-${var.team_id}"  # Nom dynamique pour l'adresse IP
 }
 
-output "minecraft_vm_public_ip2" {
-  value = google_compute_address.minecraft_ip-${var.team_id}.address  # Affiche l'IP publique de la VM
-}
+
