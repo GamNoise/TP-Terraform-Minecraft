ea5b85876dd4374fcd56986d20aad4ffd49d1f2e
BaptisteMorvilliers <baptiste.morvilliers@gmail.com>
2025-02-18 10:39:29 +0100

    version fonctionnel v2

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,39 +1,37 @@
-# Configuration du fournisseur Google Cloud
 provider "google" {
-  project = "bionic-union-451212-m1"  # ID du projet Google Cloud
-  region  = "europe-west9"            # Région Google Cloud
+  project = var.project_id
+  region  = "europe-west9"
 }
 
-# Ressource pour créer une instance de machine virtuelle Google Compute Engine
 resource "google_compute_instance" "minecraft_vm" {
-  name         = "minecraft-server"   # Nom de l'instance
-  machine_type = "e2-medium"          # Type de machine
-  zone         = "europe-west9"     # Zone de l'instance
-  image        = "projects/bionic-union-451212-m1/global/images/minecraft-server-image"  # Image de l'instance
+  name         = "minecraft-server-${var.team_id}"  # Nom dynamique basé sur team_id
+  machine_type = "e2-medium"
+  zone         = var.zone
 
-  # Configuration de l'interface réseau
+  boot_disk {
+    initialize_params {
+      image = "projects/bionic-union-451212-m1/global/images/image-srv-min"  # Image de l'instance
+    }
+  }
+
   network_interface {
     network = "default"  # Réseau par défaut
-    access_config {
-      nat_ip = google_compute_address.minecraft_ip.address  # Adresse IP NAT
-    }
+    access_config {}     # Configuration de l'IP publique
   }
 
   tags = ["minecraft"]  # Tags pour l'instance
 }
 
-# Ressource pour créer une adresse IP statique
 resource "google_compute_address" "minecraft_ip" {
-  name = "minecraft-ip"  # Nom de l'adresse IP
+  name = "minecraft-ip-${var.team_id}"  # Nom dynamique pour l'adresse IP
 }
 
-# Ressource pour ajouter l'adresse IP à Firestore via une fonction Cloud
 resource "null_resource" "add_ip_to_firestore" {
   provisioner "local-exec" {
     command = <<EOT
       curl -X POST https://REGION-PROJECT_ID.cloudfunctions.net/addIpToFirestore \
       -H "Content-Type: application/json" \
-      -d '{"ip_address": "${google_compute_address.minecraft_ip.address}", "team_id": "team-id"}'
+      -d '{"ip_address": "${google_compute_address.minecraft_ip.address}", "team_id": "${var.team_id}"}'
     EOT
   }
 
@@ -42,7 +40,6 @@ resource "null_resource" "add_ip_to_firestore" {
   ]
 }
 
-# Sortie de l'adresse IP publique de l'instance Minecraft
 output "minecraft_vm_public_ip" {
-  value = google_compute_address.minecraft_ip.address  # Valeur de l'adresse IP
+  value = google_compute_address.minecraft_ip.address  # Affiche l'IP publique de la VM
 }
